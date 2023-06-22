09bff9e8aa700d1a802066124a12b85410c8f768
eXpire163 <github@mms2k.de>
2023-06-22 22:58:31 +0200

    adds buld / image galery option

--- README.md
+++ README.md
@@ -31,6 +31,23 @@ You can use built file like:
     options: "-D var=value -hardwarnings"
 ```
 
+## Image galery
+
+If you store multiple `.scad` files in one repo, just path the directory and it will create the images a `readme.md` file for you.
+
+```yaml
+runs-on: ubuntu-latest
+steps:
+  - name: Setup
+    uses: actions/checkout@v2
+  - name: Create galery
+    uses: flat35hd99/openscad-actions@main
+    with:
+      scad_file: test/
+      output_file: output/
+      target_format_for_bulk: png
+```
+
 This action use [OpenSCAD CLI](https://en.wikibooks.org/wiki/OpenSCAD_User_Manual/Using_OpenSCAD_in_a_command_line_environment).
 
 
@@ -38,7 +55,14 @@ This action use [OpenSCAD CLI](https://en.wikibooks.org/wiki/OpenSCAD_User_Manua
 
 To run the action locally, you can call the entrypoint script as well but openscad needs to be installed before.
 
+single test
 ```
-./entrypoint.sh ./test/test.scad test.png "-D size1=10 -D size2=100"
+./entrypoint.sh ./test/test.scad test.png "-D size1=10 -D size2=100" ""
 ```
+bulk test
+```
+./entrypoint.sh ./test/ ./output/ "-D size1=10 -D size2=100" png
+```
+
+
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,40 @@
 input_file="${1}"
 output_file="${2}"
 options="${3}"
+target_format_for_bulk="${4}"
 
-openscad $options -o $output_file $input_file
+
+
+# check if input is file or directory
+if [[ -d $input_file && -d $output_file ]]; then
+    echo "$input_file and $output_file are directories, runnung bulk mode"
+    readme=$output_file/README.md
+
+    if [ "$target_format_for_bulk" == "png" ]; then
+        echo "You are creating images, so i create a readme file for you as a galery. Just move it to parent directory and the links will work"
+        echo "# OpenScad Projects" > $readme
+        echo "| Image | Filename |" >> $readme
+        echo "| --- | --- |" >> $readme
+    fi
+
+    for filename in $input_file/*.scad; do
+        echo $filename
+        shortname=$output_file/$(basename $filename).$target_format_for_bulk
+        echo $shortname
+        openscad $options -o $shortname $filename
+
+        if [ "$target_format_for_bulk" == "png" ]; then
+            echo "| ![]($shortname) | [$filename]($filename) |" >> $readme
+        fi
+
+    done
+
+elif [[ -f $input_file ]]; then
+    echo "$input_file is a single file, target format will be used from output_file"
+    openscad $options -o $output_file $input_file
+else
+    echo "$input_file $output_file is not valid, must be both a directory or both a file"
+    exit 1
+fi
+
+
